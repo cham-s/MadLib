5442aee4394bb368df87cd7578e77491751f6e3e
cham-s <cattouma@student.42.fr>
2016-09-30 18:38:54 +0200

    intial commit

---
+++ MadLib/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MadLib
+//
+//  Created by gecko on 30/09/16.
+//  Copyright © 2016 StellarTech Media. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MadLib/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MadLib
+//
+//  Created by gecko on 30/09/16.
+//  Copyright © 2016 StellarTech Media. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
